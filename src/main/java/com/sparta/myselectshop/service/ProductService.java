a6be788653127b1c9bfa4b082220ed4e8c4fd0e2
ParkDongSeob <bagd0445@gmail.com>
2024-02-17 00:56:00 +0900

    Mockito 사용

--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -16,8 +16,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -119,3 +117,4 @@ public class ProductService {
 
     }
 }
+
